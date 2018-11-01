34e68a48c8ec60d701939501ec9d232850cc1466
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-11-01 15:47:51 +0000

    e888d99 more reset to before

--- cjs/circular-progress/CircularProgress.js
+++ cjs/circular-progress/CircularProgress.js
@@ -25,6 +25,7 @@ var _CircularProgress2 = _interopRequireDefault(_CircularProgress);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function getSizes(large, small) {
+    // Size calculations for the MUI Circular Progress (https://github.com/callemall/material-ui/releases/tag/v0.16.0-rc1)
     var defaultMaterialUISize = 59.5; // Represents the pre 0.16 values size value 1
     var defaultMaterialUIMargin = 5.25; // Represents the pre 0.16 values size value 1
 
