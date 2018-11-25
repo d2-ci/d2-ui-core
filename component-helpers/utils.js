135563e4e6d81335b00bb7707fd91b8da275444d
joakimia <joakimamundsen91@gmail.com>
2018-11-25 21:08:32 +0000

    7d51e9d remove minWidth and width rules, change to 'flexBasis' also set maxHeight to 19% of total viewport

---
+++ component-helpers/utils.js
@@ -0,0 +1,16 @@
+/**
+ * @module lib/utils
+ */
+
+/**
+ * Function to create CSS class names
+ *
+ * @param {String} name The base name
+ * @param {String} selector string to append to the base name
+ * @returns String
+ */
+export var createClassName = function createClassName() {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+  var selector = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+  return selector ? name + ' ' + name + '-' + selector : name;
+};
---
+++ feedback-snackbar/FeedbackSnackbarTypes.js
@@ -0,0 +1,5 @@
+export var LOADING = 'LOADING';
+export var SUCCESS = 'SUCCESS';
+export var WARNING = 'WARNING';
+export var ERROR = 'ERROR';
+export var ACTION_MESSAGE = 'ACTION_MESSAGE';
