f7d109bdf9e0ba121330f92eb3c39c2821973446
Ilya Nee <neeilya@mail.ru>
2018-11-27 12:05:35 +0000

    f8ff8c8 Fix styles

---
+++ messages/ErrorMessage.component.js
@@ -0,0 +1,7 @@
+import _extends from 'babel-runtime/helpers/extends';
+import Message from './Message.component';
+import mapProps from '../component-helpers/mapProps';
+
+export default mapProps(function (props) {
+  return _extends({ style: { color: 'red' } }, props);
+}, Message);
