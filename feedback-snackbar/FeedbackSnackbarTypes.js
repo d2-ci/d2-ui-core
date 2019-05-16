7f542557d144dfa4c37ea7fe9c2787e9eed20868
Jen Jones Arnesen <jennifer@dhis2.org>
2019-05-16 11:03:13 +0000

    fd1df45 v6.0.0

---
+++ feedback-snackbar/FeedbackSnackbarTypes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var LOADING = exports.LOADING = 'LOADING';
+var SUCCESS = exports.SUCCESS = 'SUCCESS';
+var WARNING = exports.WARNING = 'WARNING';
+var ERROR = exports.ERROR = 'ERROR';
+var ACTION_MESSAGE = exports.ACTION_MESSAGE = 'ACTION_MESSAGE';
